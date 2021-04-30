513d6c78942ca95866879bf238aa50f3d6c12026
buuug7 <youpp@126.com>
2021-05-01 00:41:54 +0800

    fix sql

--- sql/2020-12-21-Init.sql
+++ sql/2020-12-21-Init.sql
@@ -2,34 +2,34 @@ create database `ask_hub` default charset utf8mb4 collate utf8mb4_unicode_ci;
 
 create table `users`
 (
-    id        varchar(191)  not null primary key,
-    name      varchar(255)  not null,
-    email     varchar(255)  not null,
-    password  varchar(255)  not null,
-    active    int default 1 not null,
-    loginFrom json          not null,
-    createdAt datetime      not null,
-    updatedAt datetime      not null,
+    id        varchar(255) not null primary key,
+    name      varchar(255) not null,
+    email     varchar(255) not null,
+    password  varchar(255) not null,
+    active    tinyint      not null default 1,
+    loginFrom json         not null,
+    createdAt datetime     not null,
+    updatedAt datetime     not null,
 
     constraint `unique_users_email` unique (email)
 );
 
 create table `questions`
 (
-    id          varchar(191)  not null primary key,
+    id          varchar(255)  not null primary key,
     title       varchar(255)  not null,
     description varchar(1024) not null,
     active      tinyint       not null default 1,
     createdAt   datetime      not null,
     updatedAt   datetime      not null,
-    userId      varchar(191)  not null,
+    userId      varchar(255)  not null,
 
     constraint `fk_questions_userId` foreign key (userId) references users (id)
 );
 
 create table `tags`
 (
-    id        varchar(191) not null primary key,
+    id        varchar(255) not null primary key,
     name      varchar(255) not null,
     slug      varchar(255) not null,
     createdAt datetime     not null,
@@ -38,13 +38,13 @@ create table `tags`
 
 create table `answers`
 (
-    id         varchar(191) not null primary key,
+    id         varchar(255) not null primary key,
     text       text         not null,
     active     tinyint      not null default 1,
     createdAt  datetime     not null,
     updatedAt  datetime     not null,
-    questionId varchar(191) not null,
-    userId     varchar(191) not null,
+    questionId varchar(255) not null,
+    userId     varchar(255) not null,
 
     constraint `fk_answers_questionId` foreign key (questionId)
         references questions (id) on update cascade on delete no action,
@@ -56,8 +56,8 @@ create table `answers`
 
 create table `questions_tags`
 (
-    questionId varchar(191) not null,
-    tagId      varchar(191) not null,
+    questionId varchar(255) not null,
+    tagId      varchar(255) not null,
 
     primary key (questionId, tagId),
     constraint `fk_questions_tags_questionId` foreign key (questionId)
@@ -68,8 +68,8 @@ create table `questions_tags`
 
 create table `answers_users_star`
 (
-    answerId varchar(191) not null,
-    userId   varchar(191) not null,
+    answerId varchar(255) not null,
+    userId   varchar(255) not null,
 
     primary key (answerId, userId),
     constraint `fk_answers_users_star_answerId` foreign key (answerId)
@@ -80,8 +80,8 @@ create table `answers_users_star`
 
 create table questions_users_watch
 (
-    questionId varchar(191) not null,
-    userId     varchar(191) not null,
+    questionId varchar(255) not null,
+    userId     varchar(255) not null,
 
     primary key (questionId, userId),
     constraint `fk_questions_users_watch_questionId` foreign key (questionId)
